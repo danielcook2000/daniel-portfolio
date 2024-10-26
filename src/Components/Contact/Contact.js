893f5b8674932a47c4e9deac05e3e0962dd9b685
danielcook2000 <dnlportfolio@gmail.com>
2024-10-26 15:03:50 +0530

    Add files via upload

---
+++ src/Components/Contact/Contact.js
@@ -0,0 +1,89 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import {
+  AiFillGithub,
+  AiOutlineTwitter,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+import PhoneIcon from "@mui/icons-material/Phone";
+import EmailIcon from "@mui/icons-material/Email";
+import PlaceIcon from "@mui/icons-material/Place";
+
+function Contact() {
+  return (
+    <section>
+      <Container fluid className="contact-section">
+        <Container>
+          <Row>
+            <Col md={12} className="home-about-social">
+              <h1>
+                Feel free to <span className="purple">connect </span>with me
+              </h1>
+              <div className="contact">
+                <div>
+                  <PhoneIcon
+                    color="secondary"
+                    className="con_ico"
+                    sx={{ fontSize: 45 }}
+                  />
+                  <a href="tel:+919629890481" className="con-det">
+                    +91 96298 90481
+                  </a>
+                </div>
+                <div>
+                  <EmailIcon
+                    className="con_ico"
+                    color="secondary"
+                    sx={{ fontSize: 45 }}
+                  />
+                  <a
+                    href="mailto:danielsanthiyagu@gmail.com"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="con-det"
+                  >
+                    danielsanthiyagu@gmail.com
+                  </a>
+                </div>
+                <div>
+                  <PlaceIcon
+                    color="secondary"
+                    className="con_ico"
+                    sx={{ fontSize: 45 }}
+                  />
+                  <p className="con-det">Trichy, India</p>
+                </div>
+              </div>
+              <h1>FIND ME ON</h1>
+              <ul className="home-about-social-links">
+                <li className="social-icons">
+                  <a
+                    href="https://www.linkedin.com/in/daniel-s-b8a76a216/"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="icon-colour  home-social-icons"
+                  >
+                    <FaLinkedinIn />
+                  </a>
+                </li>
+                <li className="social-icons">
+                  <a
+                    href="https://www.instagram.com/da__ni__el_?igsh=anhoMWo4NmdtcWNh"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="icon-colour home-social-icons"
+                  >
+                    <AiFillInstagram />
+                  </a>
+                </li>
+              </ul>
+            </Col>
+          </Row>
+        </Container>
+      </Container>
+    </section>
+  );
+}
+
+export default Contact;
