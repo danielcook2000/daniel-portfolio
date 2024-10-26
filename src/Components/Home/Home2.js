6edd79a7b69ecd2387e012ac3c4efce25fdc5655
danielcook2000 <dnlportfolio@gmail.com>
2024-10-26 15:04:44 +0530

    Add files via upload

---
+++ src/Components/Home/Home2.js
@@ -0,0 +1,115 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import myImg from "../../Assets/avatar.jpeg";
+import Tilt from "react-parallax-tilt";
+import {
+  AiFillGithub,
+  AiOutlineTwitter,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+import PhoneIcon from "@mui/icons-material/Phone";
+import EmailIcon from "@mui/icons-material/Email";
+import PlaceIcon from "@mui/icons-material/Place";
+
+function Home2() {
+  return (
+    <Container fluid className="home-about-section" id="about">
+      <Container>
+        <Row>
+          <Col md={8} className="home-about-description">
+            <h1 style={{ fontSize: "2.6em" }}>
+              LET ME <span className="purple"> INTRODUCE </span> MYSELF
+            </h1>
+            <p className="home-about-body">
+              I'm a creative, detail-oriented Pastry Cook with over 3 years of
+              experience crafting high-quality pastries, desserts, and baked
+              goods.
+              <br />
+              Aspiring pastry chef with a strong interest in the art of baking
+              and pastry design. Eager to leverage knowledge gained from a
+              Professional Bachelor degree in
+              <i>
+                <b className="purple"> Hotel Management </b>
+              </i>
+              <br />I aim to contribute my energy and enthusiasm to a dynamic
+              pastry team while enhancing my skills in
+              <b className="purple"> baking</b>
+            </p>
+          </Col>
+          <Col md={4} className="myAvtar">
+            <Tilt>
+              <img src={myImg} className="img-fluid" alt="avatar" />
+            </Tilt>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12} className="home-about-social">
+            <h1>
+              Feel free to <span className="purple">connect </span>with me
+            </h1>
+            <div className="contact">
+              <div>
+                <PhoneIcon
+                  color="secondary"
+                  className="con_ico"
+                  sx={{ fontSize: 45 }}
+                />
+                <a href="tel:+919629890481" className="con-det">
+                  +91 96298 90481
+                </a>
+              </div>
+              <div>
+                <EmailIcon
+                  className="con_ico"
+                  color="secondary"
+                  sx={{ fontSize: 45 }}
+                />
+                <a
+                  href="mailto:danielsanthiyagu@gmail.com"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="con-det"
+                >
+                  danielsanthiyagu@gmail.com
+                </a>
+              </div>
+              <div>
+                <PlaceIcon
+                  color="secondary"
+                  className="con_ico"
+                  sx={{ fontSize: 45 }}
+                />
+                <p className="con-det">Trichy, India</p>
+              </div>
+            </div>
+            <h1>FIND ME ON</h1>
+            <ul className="home-about-social-links">
+              <li className="social-icons">
+                <a
+                  href="https://www.linkedin.com/in/daniel-s-b8a76a216/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <FaLinkedinIn />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.instagram.com/da__ni__el_?igsh=anhoMWo4NmdtcWNh"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour home-social-icons"
+                >
+                  <AiFillInstagram />
+                </a>
+              </li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+export default Home2;
